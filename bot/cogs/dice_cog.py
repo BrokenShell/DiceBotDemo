791c5ffeb7fe9ff2749d130a966e8ab00a5fefc2
BrokenShell <webmaster@sharpdesigndigital.com>
2022-11-22 10:59:47 -0800

    part 2

--- bot/cogs/dice_cog.py
+++ bot/cogs/dice_cog.py
@@ -14,10 +14,18 @@ class Dice(commands.Cog):
 
 
 class DiceParser(commands.Cog):
+    """ Random Engine: Fortuna """
 
     @commands.command()
-    async def roll(self, ctx, dice_expr):
-        await ctx.send(f"{ctx.author.name} rolls `{dice_expr}` => {parse_dice(dice_expr)}")
+    async def roll(self, ctx, *, dice_expression: str = 'd20'):
+        """ Evaluates dice_expression: d6, 3d6 or 3d6+1
+        Examples:
+            /roll       -> rolls one twenty-sided die
+            /roll d10   -> rolls one ten-sided die
+            /roll 3d6   -> rolls three six-sided dice
+            /roll 8d6+3 -> rolls eight six-sided dice, then adds three
+            /roll XdY+Z -> rolls a Y-sided dice X times, then adds Z """
+        await ctx.send(f"{ctx.author.name} rolls {parse_dice(dice_expression)}")
 
 
 async def setup(bot):
--- bot/cogs/dice_parser.py
+++ bot/cogs/dice_parser.py
@@ -1,10 +1,41 @@
 from Fortuna import dice
 
 
-def parse_dice(dice_expr):
+def parse_dice(dice_expression):
+    if "+" in dice_expression:
+        op = "+"
+        dice_expression, modifier = dice_expression.split(op)
+        modifier = int(modifier)
+    elif "-" in dice_expression:
+        op = "-"
+        dice_expression, modifier = dice_expression.split(op)
+        modifier = abs(int(modifier))
+    else:
+        modifier = ""
+        op = ""
 
-    rolls, sides = dice_expr.split("d")
-    rolls = int(rolls) if rolls else 1
-    sides = int(sides) if sides else 20
+    if "d" in dice_expression:
+        rolls, sides = dice_expression.split('d')
+        rolls = int(rolls) if rolls else 1
+        sides = int(sides) if sides else 20
+    else:
+        rolls = 1
+        sides = int(dice_expression)
 
-    return dice(rolls, sides)
+    if op == "+":
+        output = {
+            "Expression": f'`{rolls if rolls != 1 else ""}d{sides}+{modifier}`',
+            "Roll": f'{dice(rolls, sides) + modifier}',
+        }
+    elif op == '-':
+        output = {
+            "Expression": f'`{rolls if rolls != 1 else ""}d{sides}-{modifier}`',
+            "Roll": f'{dice(rolls, sides) - modifier}',
+        }
+    else:
+        output = {
+            "Expression": f'`{rolls if rolls != 1 else ""}d{sides}`',
+            "Roll": f'{dice(rolls, sides)}',
+        }
+
+    return f"{output['Expression']} => {output['Roll']}"
